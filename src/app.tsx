27065225e805724b1b212075f33857f20fa8242a
sebastianstupak <sebastian.stupak@maethril.net>
2025-09-15 00:01:04 +0200

    hashrouter

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Label from "./pages/label";
 import NotFound from "./pages/not-found";
